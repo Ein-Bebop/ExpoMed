8265c009da4da50902b57bb21b8c86fe4ae0036d
Diego Garay <diegogv840@hotmail.com>
2021-07-26 19:53:35 -0500

    Añadido parte del Header Responsive con Función de cerrar y abrir menú, añadida carpeta Js con un archivo Header.js para funciones de header

---
+++ js/header.js
@@ -0,0 +1,7 @@
+function openM(){
+    document.getElementById("header-bar").style.transform = "translateX(0%)";
+}
+
+function closeM(){
+    document.getElementById("header-bar").style.transform = "translateX(100%)";
+}
